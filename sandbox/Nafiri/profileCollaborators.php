bc0fef6ab850160d6d61bb2b7e53a756416ac4a6
Nafiri Kusumakaulika <nkusumak@gmail.com>
2013-11-13 15:45:17 -0800

    New Ones1

--- sandbox/Nafiri/profileCollaborators.php
+++ sandbox/Nafiri/profileCollaborators.php
@@ -1,4 +1,15 @@
 <?php
 require_once "includes/config.php";
+
+//Need to get a list of friends.
+$friends = array();
+$friends[]["id"]=56;
+$friends[]["name"]="Misty";
+//echo "Friends: ".$friend["id"].", ".$friend[]["name"];
+
+//list of whether or not friends are already collaborators.
+
+
+//var_dump($friends);
 include "profileCollaborators.phtml";
 ?>
