b73c31fa626aa31beab2a5fc3c9a9e6cb68c9ec3
Marie Gilbert <sarahhuffman@dhcp-v210-066.vp.reshsg.uci.edu>
2013-11-17 09:24:49 -0800

    more on Photo class

--- sandbox/marie/dbrunner.php
+++ sandbox/marie/dbrunner.php
@@ -2,15 +2,11 @@
 
 // This is a list of the files I need to include to make things work.
 //include 'get_memorial_id.php';
-//require_once ('includes/classes/Database.php');
-//require_once ('includes/classes/FacebookFriend.php');
+require_once ('includes/classes/Database.php');
+require_once ('includes/classes/FacebookFriend.php');
 //require_once ('includes/classes/Memorial.php');
 //require_once ('includes/classes/Photo.php');
-//include 'includes/config.php';
-
-
-
-
+include 'includes/config.php';
 
 
 // The following are a series of calls for anything I will ever do. Comptuers are fast, it can just do all of them. kthxbai.
@@ -29,39 +25,28 @@ echo "<br>";
 
 echo "Photo class";
 // Photo Stuff - does nothing so far
-//$objPhoto = new Photo("Jack");
-//$objPhoto->sayHello();
+$whosphoto = "688307710";
+$photo_id = "need to get one";
+$memorial_id = 1;
+$objPhoto = new Photo("$whosphoto");
+$photo_vote = $objPhoto->votePhoto($photo_id, $memorial_id, $vote);
+echo "<br>";
 
 echo "FacebookFriends class";
 // FacebookFriends class
-//$facebook_user_id = "12345678";
-//$facebook_user_name = "Mr. Whiskers";
-//$epilogue_user_id = "100005789522071";
-//$objFF = new FacebookFriend();
-//$objFF->insertFriendList($facebook_user_id, $facebook_user_name, $epilogue_user_id);
+$facebook_user_id = "12345678";
+$facebook_user_name = "Mr. Whiskers";
+$epilogue_user_id = "100005789522071";
+$objFF = new FacebookFriend();
+$objFF->insertFriendList($facebook_user_id, $facebook_user_name, $epilogue_user_id);
 //$array_of_friends = $objFF->getFriendsFromFacebook($epilogue_user_id);
 //$objFF->insertFriendsIntoDatabase($epilogue_user_id);
 //$objFF->insertFriendList($facebook_user_id, $facebook_user_name, $epilogue_user_id);
-//echo "-------------------------";
+echo "<br>";
+echo "-------------------------";
 //$status = "Y"; // Y = invited, (A)ccepted invite
 //$objFF->getCollaborators($epilogue_user_id, $status, $memorial_id);
 //$objFF->updateInviteStatus($epilogue_user_id, $invite_this_friend, $status, $memorial_id);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "<br>";
 
 ?>
--- sandbox/marie/includes/classes/Photo.php
+++ sandbox/marie/includes/classes/Photo.php
@@ -28,31 +28,14 @@
 
   	public function votePhoto($photo_id, $memorial_id, $vote){
   		// update or downvote a photo to include in the memorial
+  		// right now it should just display the current vote for a photo_id
   		$query = 'SELECT vote, memorial_id FROM photo WHERE photo_id = "$photo_id" AND memorial_id = "$memorial_id"';		
   		$current_vote = $db->raw_query($query);
-  		
-		$update_this = "UPDATE `Vixen_test`.`photo` SET vote = '$status' WHERE facebook_user_id = '$invite_this_friend' AND epilogue_user_id = '$epilogue_user_id' AND memorial_id = '$memorial_id'";
-		$db->raw_query($update_this);
+  		print_r(array_values($current_vote));
+		  		
+//		$update_this = "UPDATE `Vixen_test`.`photo` SET vote = '$status' WHERE facebook_user_id = '$invite_this_friend' AND epilogue_user_id = '$epilogue_user_id' AND memorial_id = '$memorial_id'";
+//		$db->raw_query($update_this);
 	
-	
-	
-	
-	
-  		 photo_id	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	url	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	comment_id	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	likes_id	varchar(256)	latin1_swedish_ci		Yes	NULL		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	shares_id	varchar(256)	latin1_swedish_ci		Yes	NULL		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	fb_user_id	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	meaning_rank	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	photo_date	datetime			No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	Fulltext
-	caption	varchar(256)	latin1_swedish_ci		Yes	NULL		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	photo_tag	varchar(256)	latin1_swedish_ci		Yes	NULL		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	album_id	varchar(256)	latin1_swedish_ci		Yes	NULL		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	album_name	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	to_be_approved	tinyint(1)			No	0		 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	Fulltext
-	memorial_id	varchar(256)	latin1_swedish_ci		No			 Browse distinct values	 Change	 Drop	 Primary	 Unique	 Index	 Fulltext
-	vote
   		
   		
   		
