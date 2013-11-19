4ca7a302f9aaccb00d064fc127dc992955d0dcab
Marie Gilbert <sarahhuffman@dhcp-v210-066.vp.reshsg.uci.edu>
2013-11-18 16:28:54 -0800

    Photo class-ing + facebook_calls now in dbrunner only
    facebook_calls has been retired. dbrunner is my only controller

--- sandbox/marie/tempsave.php
+++ sandbox/marie/tempsave.php
@@ -92,4 +92,38 @@ function PhotoInsert() {
 //"INSERT INTO $current_database.`post` VALUES (
 //`item_id`,`post_content`,`post_date`,`like`,`share`,`comment_id`,`tagged`,`facebook_user_id`)";
 
+foreach ($user_profile["friends"]["data"] as $value)
+  {
+  global $facebook_user_id;
+  global $facebook_user_name;
+
+  $facebook_user_id =  ($value["id"]);
+  $facebook_user_name = ($value["name"]);
+  echo "$facebook_user_id and $facebook_user_name";
+
+
+  }
+
+  echo "<br>";
+
+function samplepageInsert() {
+	$link = mysql_connect('mysql2.speedypuppy.net:3306', 'Vixen_VixGrace', 'cutie', 'Vixen_test');
+     global $facebook_user_id;
+     global $facebook_user_name;
+     global $user_profile;
+     $epilogue_user_id = $user_profile["id"];
+
+
+	if (!$link) {
+		die('Could not connect: ' . mysql_error());
+	}
+
+	mysql_select_db('Vixen_test');
+	$insert_please = "INSERT INTO  `Vixen_test`.`user_friend_list` VALUES (
+'$epilogue_user_id',  '$facebook_user_id', '$epilogue_user_name', 'N', '0')";
+
+	mysql_query($insert_please, $link);
+
+}
+
 ?>
