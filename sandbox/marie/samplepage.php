a1c415415c112959ebd3c58a729bc508cd34b495
Marie Gilbert <sarahhuffman@dhcp-v056-177.mobile.uci.edu>
2013-11-20 11:17:16 -0800

    iccky sampepage - ignore this

--- sandbox/marie/samplepage.php
+++ sandbox/marie/samplepage.php
@@ -1,74 +1,31 @@
 <?php
 require_once "includes/config.php";
 
-if (Login::isLoggedIn()): ?>
+if (Login::isLoggedIn()):
 
-<h3>You</h3>
+echo "<h3>Sample Page<h3>";
 
-     <?php $user = $facebook->getUser();
-     ?>
+	$user = $facebook->getUser();
+     
 
-<img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+//<img src="https://graph.facebook.com/picture">
 
-<h3>Your User Object (/me)</h3>
+//      $fb_user_id = "/me";
+      $user_id = "688307710"; // Grace
+      $user_profile = $facebook->api($fb_user_id .'/photo');
+    
+    //  <pre><?php print_r($user_profile)? ></pre>
 
-      <?php
+else:
+     echo '  You are not Connected. Click <a href="login.php">here</a> to login.';
+endif
 
-      $fb_user_id = "/me";
-      $user_profile = $facebook->api($fb_user_id .'?fields=friends');
-      ?>
-      <pre><?php print_r($user_profile); ?></pre>
-
-<?php else: ?>
-      You are not Connected. Click <a href="login.php">here</a> to login.
-<?php endif ?>
-
-<?php
 print_r(array_values($user_profile));
-echo "--------------------" ?>
 
 
-
-<?php
-
-// this is the new stuff to try out
-  $facebook_user_id =  1;
-  $facebook_user_name = "b";
+echo "--------------------" 
 
 
-foreach ($user_profile["friends"]["data"] as $value)
-  {
-  global $facebook_user_id;
-  global $facebook_user_name;
-
-  $facebook_user_id =  ($value["id"]);
-  $facebook_user_name = ($value["name"]);
-  echo "$facebook_user_id and $facebook_user_name";
-  samplepageInsert();
-
-  }
-
-  echo "<br>";
-
-function samplepageInsert() {
-	$link = mysql_connect('mysql2.speedypuppy.net:3306', 'Vixen_VixGrace', 'cutie', 'Vixen_test');
-     global $facebook_user_id;
-     global $facebook_user_name;
-     global $user_profile;
-     $epilogue_user_id = $user_profile["id"];
-
-
-	if (!$link) {
-		die('Could not connect: ' . mysql_error());
-	}
-
-	mysql_select_db('Vixen_test');
-	$insert_please = "INSERT INTO  `Vixen_test`.`user_friend_list` VALUES (
-'$epilogue_user_id',  '$facebook_user_id', '$epilogue_user_name', 'N', '0')";
-
-	mysql_query($insert_please, $link);
-
-}
 
 
 
