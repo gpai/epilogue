74bfcd2ebbf23bb8cab6145a1f622b904e954584
whatknows <whatknows@gmail.com>
2013-11-30 15:02:59 -0800

    Removed errant dies and var_dumps. Ooops.

--- includes/classes/Database.php
+++ includes/classes/Database.php
@@ -43,8 +43,6 @@ class Database {
 	public function fetchOne($sql) {
 		$res = mysqli_query($this->connection, $sql); 
 		if (!$res) { 
-			var_dump($res);
-			die();
 			throw new Exception(mysqli_error($this->connection).". Full query: [$sql]"); 
 		};
 		$rs = $this->query($sql);
@@ -57,8 +55,6 @@ class Database {
 	public function fetchAll($query) {
 		$res = mysqli_query($this->connection, $sql); 
 		if (!$res) { 
-			var_dump($res);
-			die();
 			throw new Exception(mysqli_error($this->connection).". Full query: [$sql]"); 
 		};
 		
