714f9a1304e9de1b13036df8a8fc9d6f7dc11776
whatknows <whatknows@gmail.com>
2013-11-30 22:45:43 -0800

    Changed the config file to be more "danger danger" so people know when the facebook params are not set correctly.

--- includes/config.php
+++ includes/config.php
@@ -51,9 +51,13 @@ $config['fb']['appId'] = '665113106856066';
 $config['fb']['secret'] = '2d230b0ad9a39b85f68d4aa235d8d8ee';
 $config['fb']['fileUpload'] = false; // optional
 $config['fb']['scope'] = "read_stream, friends_likes, user_status";
-$config['fb']['redirectLogin'] = "http://duidesign.com/j/login.php"; //"http://duidesign.com/j/login.php";
-$config['fb']['redirectLogout'] = "http://duidesign.com/j/logout.php"; //"http://duidesign.com/j/login.php";
+$config['fb']['redirectLogin'] = ""; // example: "http://localhost/YOUR-SANDBOX/login.php"
+$config['fb']['redirectLogout'] = ""; //example: "http://localhost/YOUR-SANDBOX/logout.php"
 $config['fb']['cookie'] = true; // optional
+
+if ($config['fb']['redirectLogin'] == "" || $config['fb']['redirectLogout'] == "")
+	die("DANGER DANGER DANGER!!! \n You are using a config.php file that does not have the 'redirectLogin' and redirectLogout' parameters configured appropriately for your machine. Please edit your config.php file to work on your local box.");
+
 
 $config['db']['user'] = 'Vixen_VixGrace'; 
 $config['db']['password'] = 'cutie';
