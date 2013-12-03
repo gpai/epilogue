a2f1c23c8ebde3f3d143ac22ea5b580e39100966
Nafiri Kusumakaulika <nkusumak@gmail.com>
2013-12-02 22:27:06 -0800

    moar updates for Grace.

---
+++ memorial.php
@@ -0,0 +1,21 @@
+<?php
+require_once "includes/config.php";
+require_once "includes/classes/FacebookFriend.php";
+require_once "includes/classes/Registry.php";
+require_once "includes/classes/Database.php";
+require_once "includes/classes/Epilogue.php";
+require_once "includes/classes/Favorite.php";
+require_once "includes/classes/Login.php";
+require_once "includes/classes/Memorial.php";
+require_once "includes/classes/Photo.php";
+require_once "includes/classes/Session.php";
+require_once "includes/classes/User.php";
+require_once "includes/classes/Video.php";
+
+//Use Marie's function to get a list of pictures, favorites, posts, and videos.
+
+//Allow people to put stories in.
+	//Get function from Marie to insert these stories.
+
+include "pages/memorial.phtml";
+?>
